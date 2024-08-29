bc34c01755fa6be876de456f8e9e12b4f2f3cd6c
im-ilyasmohamed <im.ilyasmohamed@gmail.com>
2024-08-29 12:25:56 +0100

    Added listen.js for supabases, edited connection.js which is pool file

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
+
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
@@ -5,8 +6,17 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+// hopefull config file doesnt cause too many problems
+// This will allow a connection to the hosted database from a local machine. 
+// The config object also needs a max property to limit how many connections the pool will have available.
+const config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASEURL;
+  config.max = 2;
 }
 
-module.exports = new Pool();
+if (!process.env.PGDATABASE && process.env.DATABASEURL) {
+  throw new Error("PGDATABASE not set or DATABASE_URL not set");
+}
+
+module.exports = new Pool(config);
